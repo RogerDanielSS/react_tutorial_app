369cfdb557a43300b544a79d137da25f9e7ecfe0
RogerDanielSS <danielsimoesp@outlook.com>
2022-10-29 15:41:33 -0300

    wip: Added light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { ThemeProvider } from '@emotion/react';
+
+//fonts MUI5
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { LightTheme } from './shared/themes';
+
 //import logo from './logo.svg';
 //import './App.css';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
