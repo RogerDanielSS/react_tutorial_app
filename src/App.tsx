e33f2cdddc1bdf401cd2e6e1171e6169ea6a3585
RogerDanielSS <danielsimoesp@outlook.com>
2022-10-31 12:57:44 -0300

    wip - Added dark theme and change theme function

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
-import { ThemeProvider } from '@emotion/react';
+import { AppThemeProvider } from './shared/contexts/ThemeContext';
 
 //fonts MUI5
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { LightTheme } from './shared/themes';
 
 //import logo from './logo.svg';
 //import './App.css';
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
-    </ThemeProvider>
+      <AppThemeProvider>
+        <BrowserRouter>
+          <AppRoutes/>
+        </BrowserRouter>
+      </AppThemeProvider>
   );
 }
 
